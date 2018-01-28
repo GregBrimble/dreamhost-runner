b038035ad10d289006e6a13ecda318875957c2ce
Greg Brimble <developer@gregbrimble.com>
2018-01-27 16:13:56 -0800

    Import improvements

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,9 @@
 from importlib import import_module
 import logging
 import os
-from sys import argv, executable, exit
+from sys import argv, executable, exit, path
+
+logging.basicConfig(level=logging.DEBUG)
 
 try:
     from config import INSTANCE, INTERPRETER, MODULE, PACKAGE
@@ -9,8 +11,8 @@ except ImportError:
     exit("Could not import `config.py`. Copy from `config.example.py` and edit as required.")
 
 try:
-    application = getattr(import_module("%s.%s" % (PACKAGE, MODULE)), INSTANCE)
-    # from boilerplate_web_service.app import app as application
+    path.append(PACKAGE)
+    application = getattr(import_module(MODULE), INSTANCE)
 except ImportError:
     if os.path.relpath(executable, os.getcwd()) != INTERPRETER:
         try:
@@ -20,3 +22,5 @@ except ImportError:
             exit("Virtual environment `%s` could not be found." % INTERPRETER)
     else:
         exit("Could not import `%s` from `%s`." % (INSTANCE, "%s.%s" % (PACKAGE, MODULE)))
+except OSError:
+    exit("Could not change working directory to `%s`." % PACKAGE)
